791cd2cf4cf2f3a1e8bfa97fe1bef06d3642e03a
Nick Kaufmann <coding.saints2015@gmail.com>
2017-03-20 13:19:09 -0400

    regex with params

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import { BrowserRouter as Router,
 const App = (props) => (
     <Router>
     <div>
-    <Route path="/:page?/:subpage?" render={({match}) => (
+    <Route path="/:a(\d{2}-\d{2}-\d{4})/:b(.[a-z]+)"
+     render={({match}) => (
 <h1>
-PAGE: {match.params.page || 'Home'} <br />
-SUBPAGE: {match.params.subpge}
+paramA: {match.params.a} <br />
+paramB: {match.params.b}
 </h1>
 )} />
 </div>
